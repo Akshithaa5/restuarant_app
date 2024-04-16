62028b3c3f130ea53d412ef6fefcb2daf16e0c4a
Akshithaa5 <jessierowe143@gmail.com>
2024-04-16 22:55:12 +0530

    Add Restaurant Summary

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import Header from "./Component/Layout/Header";
 
 import { Fragment } from 'react';
 
+import Meals from "./Component/Meals/Meals";
 
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
