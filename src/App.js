2e9437ee0ec5fa772e6f91db2deefdc8b60e0aca
Akshithaa5 <jessierowe143@gmail.com>
2024-04-17 14:55:14 +0530

    Making a hard Coded Cart without any Functionality

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Header from "./Component/Layout/Header";
 import { Fragment } from 'react';
 
 import Meals from "./Component/Meals/Meals";
-
+import Cart from "./Component/Cart/Cart";
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
